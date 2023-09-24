326782c6f0070846f73f33dab7ec6ccf7985a650
Harpuneet-Tiwana <145626273+Harpuneet-Tiwana@users.noreply.github.com>
2023-09-24 16:32:19 -0400

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -20,8 +20,6 @@ public class GamePanel extends JPanel implements ActionListener {
     boolean running = false;
     Timer timer;
     Random random;
-    JButton restartButton;
-    JPanel panel;
 
     GamePanel() {
         random = new Random();
@@ -130,21 +128,15 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void gameOver(Graphics g) {
-//        g.setColor(Color.red);
-//        g.setFont( new Font("Ink Free",Font.BOLD, 40));
-//        FontMetrics metrics1 = getFontMetrics(g.getFont());
-//        g.drawString("Score: "+applesEaten, (SCREEN_WIDTH - metrics1.stringWidth("Score: "+applesEaten))/2, g.getFont().getSize());
-//        //Game Over text
-//        g.setColor(Color.red);
-//        g.setFont( new Font("Ink Free",Font.BOLD, 75));
-//        FontMetrics metrics2 = getFontMetrics(g.getFont());
-//        g.drawString("Game Over", (SCREEN_WIDTH - metrics2.stringWidth("Game Over"))/2, SCREEN_HEIGHT/2);
-        // Restart
-        panel = new JPanel();
-        restartButton = new JButton("Restart");
-        this.add(panel);
-        this.add(restartButton);
-        restartButton.addActionListener(e -> new GameFrame());
+       g.setColor(Color.red);
+       g.setFont( new Font("Ink Free",Font.BOLD, 40));
+       FontMetrics metrics1 = getFontMetrics(g.getFont());
+       g.drawString("Score: "+applesEaten, (SCREEN_WIDTH - metrics1.stringWidth("Score: "+applesEaten))/2, g.getFont().getSize());
+       //Game Over text
+       g.setColor(Color.red);
+       g.setFont( new Font("Ink Free",Font.BOLD, 75));
+       FontMetrics metrics2 = getFontMetrics(g.getFont());
+       g.drawString("Game Over", (SCREEN_WIDTH - metrics2.stringWidth("Game Over"))/2, SCREEN_HEIGHT/2);
     }
 
     @Override
